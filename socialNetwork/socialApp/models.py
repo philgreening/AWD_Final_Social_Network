52719f58c42c3195faff7b4bbd7c81ccf0b48714
Phil Greening <philgreening@hotmail.com>
2022-02-21 01:09:21 +0000

    first commit. Created Django and Vue apps

---
+++ socialNetwork/socialApp/models.py
@@ -0,0 +1,25 @@
+from distutils.command.upload import upload
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    bio = models.TextField(max_length=200, blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images', blank=True)
+    followers = models.ManyToManyField(User, blank=True, related_name='followers')
+
+    def __str__(self):
+        return self.user.username
+
+class Posts(models.Model):
+    postId = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    post_date = models.DateField(auto_now_add=True)
+    post_text = models.CharField(max_length=250)
+    likes = models.IntegerField(null=True)
+    images = models.ImageField(upload_to='post_images')
+
---
+++ socialnetwork_vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="wrapper">
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <div class="navbar-brand">
+            <router-link to="/" class="navbar-brand"><strong>Social Network</strong></router-link>
+          </div>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/feed" class="nav-link"><strong>Feed</strong></router-link>
+            </li>
+
+            <li class="nav-item">
+              <router-link to="/log-in" class="btn btn-light"><strong>Log In</strong></router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../node_modules/bootstrap'
+</style>
