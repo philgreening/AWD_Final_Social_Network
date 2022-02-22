b84613110692a1fcbae1c189144e4f9ae4e3ce3b
Phil Greening <philgreening@hotmail.com>
2022-02-22 00:17:46 +0000

    started work on signup page

---
+++ socialnetwork_vue/src/views/SignUp.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-4 m-4">
+                <h1 class="text-center p-4">Sign Up</h1>
+                <form @submit.prevent="submitForm">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Username</span>
+                        </div>
+                        <input type="text" class="form-control" v-model="username">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="password">Password</span>
+                        </div>
+                        <input type="text" class="form-control" v-model="password">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="password2">Repeat Password</span>
+                        </div>
+                        <input type="text" class="form-control" v-model="password2">
+                    </div>
+                    <div class="alert alert-danger mb-3" v-if="errors.length">
+                        <p class="text-center" v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                    </div>
+                    <button type="submit" class="btn btn-primary pull-left">Sign Up</button>
+
+                    <hr>
+                    Or <router-link to="/log-in">click here</router-link> to log in
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'SignUp',
+  data () {
+      return {
+          username: '',
+          password: '',
+          password2: '',
+          errors: []
+      }
+  },
+  methods: {
+      submitForm () {
+          this.errors = []
+          console.log(this.errors);
+
+          if(this.username === ''){
+              this.errors.push('Please enter a username')
+          }
+          if(this.password === ''){
+              this.errors.push('Please enter a password')
+          }
+          if(this.password !== this.password2){
+              this.errors.push('the Passwords don\'t match')
+          }
+          if(!this.errors.length){
+              const formData = {
+                  username: this.username,
+                  password: this.password
+              }
+              axios
+                .post("/api/v1/users/", formData)
+                .then(Response => {
+                    this.$router.push('/log-in')
+                })
+                .catch(error => {
+                if (error.response) {
+                    for (const property in error.response.data) {
+                        this.errors.push(`${property}: ${error.response.data[property]}`)
+                    }
+                    console.log(JSON.stringify(error.response.data))
+                } else if (error.message) {
+                    this.errors.push('Something went wrong. Please try again')
+                    
+                    console.log(JSON.stringify(error))
+                }
+            })
+          }
+
+      }
+  }
+}
+</script>
