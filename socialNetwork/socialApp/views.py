a6ce481f140ddbd502651d6f613afb094244a6d0
Phil Greening <philgreening@hotmail.com>
2022-02-22 00:18:38 +0000

    started work on signup page

--- socialNetwork/socialApp/views.py
+++ socialNetwork/socialApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import *
+from .models import *
+
 # Create your views here.
